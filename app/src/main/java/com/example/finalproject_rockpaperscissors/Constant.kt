f33b1df221a119ae512381757a7db6479f2bd562
Jan Mikhail Talanay <12004259@benilde.edu.ph>
2022-08-09 11:34:50 +0800

    Main Activity Done

---
+++ app/src/main/java/com/example/finalproject_rockpaperscissors/Constant.kt
@@ -0,0 +1,4 @@
+package com.example.finalproject_rockpaperscissors
+
+const val REQUEST_CODE = 1
+const val USER = "user"
--- app/src/main/java/com/example/finalproject_rockpaperscissors/GameActivity.kt
+++ app/src/main/java/com/example/finalproject_rockpaperscissors/GameActivity.kt
@@ -2,10 +2,21 @@ package com.example.finalproject_rockpaperscissors
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.finalproject_rockpaperscissors.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGameBinding
+    private lateinit var extra: String
+    private val txtUser by lazy { binding.txtUsername}
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
+        binding = ActivityGameBinding.inflate(layoutInflater)
+//        setContentView(R.layout.activity_game)
+        setContentView(binding.root)
+
+        extra = intent.getStringExtra(USER) ?: getString(R.string.UserName)
+        txtUser.text = extra
     }
 }
